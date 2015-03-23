5c6e7ec427372a4b0b7e7babbbfd5665192a402c
jogi <jogendra.lanet@gmail.com>
2015-03-23 15:30:46 +0530

    Cmmiting adapter

---
+++ Myapp/src/com/example/myapp/DrawerListAdapter.java
@@ -0,0 +1,58 @@
+package com.example.myapp;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class DrawerListAdapter extends BaseAdapter {
+	private Context context;
+	private ArrayList<String> navDrawerItems;
+
+	public DrawerListAdapter(Context context, ArrayList<String> navDrawerItems) {
+		super();
+		this.context = context;
+		this.navDrawerItems = navDrawerItems;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return navDrawerItems.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return navDrawerItems.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+
+		if (convertView == null) {
+			LayoutInflater mInflater = (LayoutInflater) context
+					.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+			convertView = mInflater.inflate(R.layout.drawer_layout_list, null);
+		}
+
+		TextView title = (TextView) convertView.findViewById(R.id.textView1);
+		title.setText("ITEM" + position);
+		Log.i("ITEm", navDrawerItems.get(position));
+		return convertView;
+	}
+
+}
